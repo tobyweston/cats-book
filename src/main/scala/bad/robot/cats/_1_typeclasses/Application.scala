ad07b5d01a578f47115b57ebe79f5c12a0b24e9d
Toby <toby.weston@gmail.com>
2019-10-28 10:53:41 +0000

    Cat with a Printable[Cat]

---
+++ src/main/scala/bad/robot/cats/_1_typeclasses/Application.scala
@@ -0,0 +1,17 @@
+package bad.robot.cats._1_typeclasses
+
+final case class Cat(name: String, age: Int, colour: String)
+
+object Cat {
+  implicit val printable: Printable[Cat] = new Printable[Cat] {
+    def format(cat: Cat): String = s"${cat.name} is a ${cat.age} year-old ${cat.colour} cat"
+  }
+}
+
+object Application extends App {
+  
+  val cat = Cat("Garfield", 5, "Orange and black")
+
+  Printable.print(cat)
+}
+
