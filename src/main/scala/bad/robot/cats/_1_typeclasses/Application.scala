63f2017826604d9744c5c8461bd7dda18ce24abf
Toby <toby.weston@gmail.com>
2019-10-28 11:05:44 +0000

    use the type class instances to define how to represent basic types as a string, bit superfluous here but demonstrative of the technique

--- src/main/scala/bad/robot/cats/_1_typeclasses/Application.scala
+++ src/main/scala/bad/robot/cats/_1_typeclasses/Application.scala
@@ -3,19 +3,25 @@ package bad.robot.cats._1_typeclasses
 final case class Cat(name: String, age: Int, colour: String)
 
 object Cat {
+  import PrintableInstances._
+
   implicit val printable: Printable[Cat] = new Printable[Cat] {
-    def format(cat: Cat): String = s"${cat.name} is a ${cat.age} year-old ${cat.colour} cat"
+    def format(cat: Cat): String = {
+      val name = Printable.format(cat.name)
+      val age = Printable.format(cat.age)
+      val colour = Printable.format(cat.colour)
+      s"$name is a $age year-old $colour cat"
+    }
   }
 }
 
 object Application extends App {
-  
+
   val cat = Cat("Garfield", 5, "Orange and black")
 
   Printable.print(cat)
-  
+
   import PrintableSyntax._
-  
+
   cat.print
 }
-
