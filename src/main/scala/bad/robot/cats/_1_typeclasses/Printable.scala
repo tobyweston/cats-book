1d6f0fb6ac424f23bfe67250de053442d0d75bd6
Toby <toby.weston@gmail.com>
2019-10-28 10:36:54 +0000

    Printable type class, interface object and instances

---
+++ src/main/scala/bad/robot/cats/_1_typeclasses/Printable.scala
@@ -0,0 +1,24 @@
+package bad.robot.cats._1_typeclasses
+
+// the type class
+trait Printable[A] {
+  def format(value: A): String
+}
+
+// type class "interfaces" (as in contracts)
+// two typical ways to do this. 1) interface object (as in this one) and interface syntax (implicit classes to extend a type)
+object Printable {
+  def format[A](a: A)(implicit printable: Printable[A]): String = printable.format(a)
+  def print[A](a: A)(implicit evidence: Printable[A]): Unit = println(format(a))
+}
+
+// type class instances
+object PrintableInstances {
+  implicit val printableString: Printable[String] = new Printable[String] {
+    override def format(value: String): String = value
+  }
+
+  implicit val printableInt: Printable[Int] = new Printable[Int] {
+    override def format(value: Int): String = value.toString
+  }
+}
